6d17256f1240226ab3a7adfae8b6bbb4b2491beb
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-08-04 23:07:41 +0700

    First Scripts file

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] ghosts;
+    public GameObject pacman;
+    
+    public Transform pellets;
+
+    public int score { get; private set;}
+}
