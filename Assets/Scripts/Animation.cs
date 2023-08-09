f46c3d8865613f4d2bc9e1423b1bc00f824c009e
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-08-09 10:45:37 +0700

    complete generic animation class

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Animation : MonoBehaviour
+{
+    //change the sprite that is being displayed
+    public SpriteRenderer spriteRenderer { get; set; }
+    public Sprite[] sprites;
+    public float waitTime = 0.25f;
+    public int currentSpriteIndex { get; private set; }
+    public bool loop = true;
+
+    private void Awake() {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Start(){
+        InvokeRepeating(nameof(NextFrame), this.waitTime, this.waitTime);
+    }
+
+    private void changeSprite(){
+        this.spriteRenderer.sprite = this.sprites[this.currentSpriteIndex];
+    }
+
+    private void NextFrame(){
+        //if we have no sprites, return
+        if(!this.spriteRenderer.enabled){
+            return;
+        }
+
+        this.currentSpriteIndex++;
+        //print(this.currentSpriteIndex);
+
+        //if we are at the end of the array
+        if(this.currentSpriteIndex >= this.sprites.Length){
+            //if we are looping
+            if(this.loop){
+                //reset the index
+                this.currentSpriteIndex = 0;
+            }
+            else{
+                //stop the animation
+                CancelInvoke(nameof(NextFrame));
+                return;
+            }
+        }
+
+        //change the sprite
+        changeSprite();
+
+    }
+
+    public void restart(){
+        this.currentSpriteIndex = -1;
+        NextFrame();
+    }
+}
