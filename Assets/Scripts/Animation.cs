46bc3bbe055308d4de688846933bb498b31f8e7f
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-08-10 20:25:59 +0700

    complete generic movement script

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -4,42 +4,55 @@ using UnityEngine;
 public class Animation : MonoBehaviour
 {
     //change the sprite that is being displayed
-    public SpriteRenderer spriteRenderer { get; set; }
+    public SpriteRenderer spriteRenderer { get; private set; }
     public Sprite[] sprites;
     public float waitTime = 0.25f;
     public int currentSpriteIndex { get; private set; }
     public bool loop = true;
 
-    private void Awake() {
+    private void Awake()
+    {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
 
     }
 
-    private void Start(){
+    private void Start()
+    {
         InvokeRepeating(nameof(NextFrame), this.waitTime, this.waitTime);
     }
 
-    private void changeSprite(){
-        this.spriteRenderer.sprite = this.sprites[this.currentSpriteIndex];
+    private void changeSprite()
+    {
+        if (this.sprites.Length >= 0
+        && this.currentSpriteIndex < this.sprites.Length)
+        {
+            //print("Changing sprite to " + this.currentSpriteIndex);
+            this.spriteRenderer.sprite = this.sprites[this.currentSpriteIndex];
+        }
+
     }
 
-    private void NextFrame(){
+    private void NextFrame()
+    {
         //if we have no sprites, return
-        if(!this.spriteRenderer.enabled){
+        if (!this.spriteRenderer.enabled)
+        {
             return;
         }
 
         this.currentSpriteIndex++;
-        //print(this.currentSpriteIndex);
 
         //if we are at the end of the array
-        if(this.currentSpriteIndex >= this.sprites.Length){
+        if (this.currentSpriteIndex >= this.sprites.Length)
+        {
             //if we are looping
-            if(this.loop){
+            if (this.loop)
+            {
                 //reset the index
                 this.currentSpriteIndex = 0;
             }
-            else{
+            else
+            {
                 //stop the animation
                 CancelInvoke(nameof(NextFrame));
                 return;
@@ -51,7 +64,8 @@ public class Animation : MonoBehaviour
 
     }
 
-    public void restart(){
+    public void restart()
+    {
         this.currentSpriteIndex = -1;
         NextFrame();
     }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+//must have a rigidbody2d component
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    //settings
+    public float speed = 8.0f;
+    public float speedMultiplier = 1.0f;
+    public Vector2 initDirection;
+    public LayerMask obstacleLayer; //obstacles to avoid
+
+    // "new" keyword hides the inherited member
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Vector2 direction { get; private set; }
+    public Vector2 nextDirection { get; private set; } //queue up the direction - move in this direction when possible
+    //easier to control pacman with keyboard
+    public Vector3 startPosition { get; private set; }
+    
+
+    private void Awake(){
+        //reference some initial components
+        this.rigidbody = GetComponent<Rigidbody2D>();
+        this.startPosition = this.transform.position;
+    }
+
+    private void Start(){
+        init();
+        
+    }
+    public void init(){
+        this.speedMultiplier = 1.0f;
+        this.direction = this.initDirection;
+        this.nextDirection = Vector2.zero;
+        this.transform.position = this.startPosition;
+        this.rigidbody.isKinematic = false;
+        this.enabled = true;
+    }
+
+    private void Update(){
+        //Time.deltaTime;
+        //continuosly try to set the direction if some direction is queued up
+        if(this.nextDirection != Vector2.zero){
+            setDirection(this.nextDirection, true);
+        }
+    }
+
+    // FixedUpdate is called once per physics frame
+    private void FixedUpdate() {
+        Vector2 curPos = this.rigidbody.position;
+        // calculate movement
+        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
+        // update position
+        this.rigidbody.MovePosition(curPos + translation);
+    }
+
+    public void setDirection(Vector2 direction, bool forced = false){
+
+        if(forced || !isBlock(direction)){
+            //if the direction is not blocked, set the direction
+            this.nextDirection = direction;
+            this.nextDirection = Vector2.zero;
+        }
+        else {
+            //if the direction is blocked, queue up the direction
+            this.nextDirection = direction;
+        }
+    }
+
+    public bool isBlock(Vector2 direction)
+    {
+        // box cast instead of raycast
+        //transform.position is the center of the box so we go half the size over in each direction
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.9f, 0.0f, direction, 1.5f, this.obstacleLayer);
+        // hit.collider is null if nothing was hit
+        // hit.collider is not null if something was hit
+        return hit.collider != null;
+    }
+}
