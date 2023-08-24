df5bd1eabc80bb0edd024e9370a7dc37113e7fca
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-08-24 20:20:19 +0700

    pellet under development

--- Assets/Scripts/Game object/Pellet.cs
+++ Assets/Scripts/Game object/Pellet.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Pellet : MonoBehaviour
 {
+    private int point = 10;
+
+    //virtual means overrideable
+    protected virtual void Eat(){
+        this.gameObject.SetActive(false);
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        //make sure the object is pacman
+        if(other.gameObject.layer == LayerMask.NameToLayer("Pacman")){
+            Eat();
+        }
     }
 }
